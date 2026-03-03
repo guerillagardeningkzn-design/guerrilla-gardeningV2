ce1ab7939067650e0b5a3014b81e06c207b6e6af
guerillagardeningkzn-design <guerillagardeningkzn@gmail.com>
2026-03-03 13:53:32 +0200

    Initial project structure + basic welcome page
    Initial project structure + basic welcome page

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("Guerrilla Gardening starting... 🌱");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page loaded!");
+  // Later: load player state, show zones, etc.
+});
